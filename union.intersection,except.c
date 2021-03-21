db5bc2a9cecaa74eab09c7c3060585fe01f36a76
Abisheikha <73419608+Abisheikha@users.noreply.github.com>
2021-03-21 19:53:42 +0530

    Update union.intersection,except.c

--- union.intersection,except.c
+++ union.intersection,except.c
@@ -1,3 +1,11 @@
+/* Find the union intersection of two list and also find except (remove even elements from list1 and odd elements from list2)
+Input 
+List 1: 1,3,4,5,6,8,9
+List 2: 1, 5,8,9,2
+Union: 1, 3,4,5,6,8,9,2
+Intersection: 1,5,8,9
+Except: 1, 3, 5,9,8,2 */
+
 #include<stdio.h>
 int func_union(int arr1[],int arr2[],int m, int n, int res[])
 {
